e6063a203dee3b16cd1f63b435fb61e63ec5b0f9
Banyc <36535895+Banyc@users.noreply.github.com>
2024-01-07 21:58:37 +0800

    feat: `GaugeHandle::total_bytes()`

--- src/gauge.rs
+++ src/gauge.rs
@@ -21,6 +21,7 @@ pub struct GaugeHandle {
     prev: Option<Instant>,
     thruput: f64,
     bytes: Arc<AtomicU64>,
+    total_bytes: u64,
 }
 impl GaugeHandle {
     fn new(alpha: NormalizedF64, bytes: Arc<AtomicU64>) -> Self {
@@ -29,20 +30,29 @@ impl GaugeHandle {
             prev: None,
             thruput: 0.0,
             bytes,
+            total_bytes: 0,
         }
     }
 
-    pub fn thruput(&mut self, now: Instant) -> f64 {
+    pub fn update(&mut self, now: Instant) {
         let d = now.duration_since(self.prev.unwrap_or(now));
         self.prev = Some(now);
         let d = d.as_secs_f64();
         if d == 0.0 {
-            return self.thruput;
+            return;
         }
         let bytes = self.bytes.swap(0, Ordering::Relaxed);
+        self.total_bytes += bytes;
         let thruput = bytes as f64 / d;
         self.thruput = self.alpha.get() * thruput + (1. - self.alpha.get()) * self.thruput;
+    }
+
+    pub fn thruput(&self) -> f64 {
         self.thruput
+    }
+
+    pub fn total_bytes(&self) -> u64 {
+        self.total_bytes
     }
 }
 
